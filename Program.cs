67cf910ffaa51ed0e203b751791c4ae844505c10
vishal.Alok <you@example.com>
2021-10-18 06:44:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    public class Cal
+    {
+        //Can change number of parameters / type of parameters
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+    public class TestMemberOverloading
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Cal.add(12, 23));
+            Console.WriteLine(Cal.add(12, 23, 25));
+        }
+    }
+}
